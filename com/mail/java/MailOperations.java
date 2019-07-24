3bd007f15cbc5359b1326263c84a27b0bc57f5ce
Yash <yashwanth.merugu@gmail.com>
2019-07-24 14:56:34 +0200

    Multiparts signing

--- com/mail/java/MailOperations.java
+++ com/mail/java/MailOperations.java
@@ -14,7 +14,6 @@ import java.util.HashMap;
 
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
-import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
@@ -27,13 +26,15 @@ import javax.mail.internet.MimeMultipart;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.pdfbox.io.IOUtils;
+import org.bouncycastle.asn1.cms.IssuerAndSerialNumber;
+import org.bouncycastle.asn1.x509.X509Name;
 import org.bouncycastle.mail.smime.SMIMESignedGenerator;
 
 @SuppressWarnings("all")
 public class MailOperations {
 	private final static Log log = LogFactory.getLog(MailOperations.class);
 	
-	private static String TO_ADDRESS = "***************";
+	private static String TO_ADDRESS = "venu-gopal-re.nomula@uniper.energy";
 	
 	public static void main(String[] args) throws IOException {
 		tetMail(false, true);
@@ -41,7 +42,7 @@ public class MailOperations {
 		// https://github.com/protocol7/smime-java-example/blob/master/src/main/java/com/protocol7/smime/Verify.java
 	}
 
-	public static boolean sendMessage = false, auth = false, useAttachementFile = true;
+	public static boolean sendMessage = false, auth = false, useAttachementFile = true, signSecretMessage = true;
 	
 	public static String secretMessage = "Confidential Information between sender and User Need to be Signed as Base64.";
 	public static void tetMail(boolean isClassPath, boolean isSigned) {
@@ -53,6 +54,9 @@ public class MailOperations {
 			Log4J.log("message failed.");
 		}
 	}
+	// signCert,privateKey,keystore,alias,signGen,signEncoder
+	static HashMap<String, Object> certs = new HashMap();
+		
 	// https://stackoverflow.com/q/24738657/5081877
 	public static boolean sendMail(String subject, String mailMessage, String recipients, boolean isSigned, boolean isClassPath) {
 		try {
@@ -69,36 +73,89 @@ public class MailOperations {
 			String header_name = "Content-ID", header_value = "<b>";
 			message.addHeader( header_name, header_value );
 			
-			if ( useAttachementFile || isSigned) {
+			
+			boolean oldPartPaerSigning = false, multiPartSign = true;
+			
+			if (multiPartSign) {
+				SMIMESignedGenerator gen = setSMIMEDigitallySignedDataGen(isClassPath);
 				MimeMultipart finalPart = new MimeMultipart();
 				
-				MimeBodyPart part1 = new MimeBodyPart();
-				part1.setText(mailMessage); // Encoding: 7bit Display Message
+				MimeMultipart signedParts = setSMIMEDigitallySignedData(isClassPath);
 				
-				finalPart.addBodyPart(part1);
+				MimeBodyPart msg = new MimeBodyPart();
+				msg.setText(mailMessage);
+				msg.addHeader("Content-ID", "Plain Text");
+				signedParts.addBodyPart(msg);
+				
+				if (signSecretMessage) {
+					X509Certificate signCert = (X509Certificate) certs.get("signCert");
+					PrivateKey privateKey_PEM = (PrivateKey) certs.get("privateKey");
+					
+					MimeBodyPart content = new MimeBodyPart();
+					String signedMsg = BC_Cryptography.getEncodedASNMessage(signCert, privateKey_PEM, secretMessage);
+					content.setText( signedMsg );
+					content.addHeader("Content-ID", "Confidential Info");
+					signedParts.addBodyPart(content);
+				}
 				
 				if (useAttachementFile) {
 					MimeBodyPart attachementPart = setAttachement();
-					
-					finalPart.addBodyPart( attachementPart );
+					attachementPart.addHeader("Content-ID", "Attachement File");
+					signedParts.addBodyPart(attachementPart);
 				}
+				// https://stackoverflow.com/q/11295760/5081877
+				finalPart = signContentParts(signedParts, isClassPath);
+				message.setContent(finalPart, finalPart.getContentType());
+				
+			} else if ( oldPartPaerSigning && (useAttachementFile || isSigned) ) {
+				MimeMultipart finalPart = new MimeMultipart();
 				
 				if (isSigned) { // Generating a pkcs7-signature message (if (signSecretMessage) {)
-					MimeMultipart signedParts = setSMIMEDigitallySignedData(isClassPath, mailMessage);
+					
+					MimeMultipart signedParts = setSMIMEDigitallySignedData(isClassPath);
 					// setSMIMEData_P7SCert(isClassPath, mailMessage);
+					
+					// Sign Attachment too. Or we can't verify Sign.
+					if (useAttachementFile) {
+						MimeMultipart signedAttachementParts = new MimeMultipart();
+						MimeBodyPart attachementPart = setAttachement();
+						
+						// Signing Part - Encoding: base64
+						signedAttachementParts = signedGen.generate(attachementPart);
+						int count = signedAttachementParts.getCount();
+						for (int index = 0; index < count; index++) {
+							MimeBodyPart signedAttPart = (MimeBodyPart) signedAttachementParts.getBodyPart(index);
+							finalPart.addBodyPart( signedAttPart );
+						}
+					}
 					
 					// Message + Attachment file + ( Secret Message + Certificate(smime.p7s) ) = Multipart
 					int count = signedParts.getCount();
 					for (int index = 0; index < count; index++) {
-						BodyPart signedPart = signedParts.getBodyPart(index);
+						MimeBodyPart signedPart = (MimeBodyPart) signedParts.getBodyPart(index);
 						finalPart.addBodyPart( signedPart );
 					}
+					
+					// Content-Type: multipart/signed; protocol="application/pkcs7-signature"; micalg=sha-256;
+				//	message.setContent(finalPart, signedParts.getContentType());
+				} else if (useAttachementFile) {
+					MimeBodyPart part1 = new MimeBodyPart();
+					part1.setText(mailMessage); // Encoding: 7bit Display Message
+					finalPart.addBodyPart(part1);
+					
+					MimeBodyPart attachementPart = setAttachement();
+					// Content-Type: application/octet-stream; name=MyServer.jks
+					finalPart.addBodyPart( attachementPart );
+					
+					// multipart/mixed = application/octet-stream + multipart/signed
+					// Content-Type: multipart/mixed; boundary="----=_Part_0_1365202186.1563878801178"
 				}
 				message.setContent(finalPart, finalPart.getContentType());
+				
+				
 			} else { //Simple Message - [Plain Text | HTML Content]
 				message.setContent( mailMessage, "text/plain" );
 			}
-      
 			message.saveChanges();
 			Log4J.log("Message is Ready to Send/Store.");
 			
@@ -132,6 +189,45 @@ public class MailOperations {
 		}
 		return false;
 	}
+	// SmimeMultipartSigned
+	private static MimeMultipart signContentParts(MimeMultipart contentParts, boolean isClassPath) throws Exception {
+		MimeBodyPart body = new MimeBodyPart();
+		body.setContent(contentParts);
+		
+		SMIMESignedGenerator gen = setSMIMEDigitallySignedDataGen(isClassPath);
+		MimeMultipart signedParts = gen.generate(body);
+		return signedParts;
+	}
+	
+	private static SMIMESignedGenerator setSMIMEDigitallySignedDataGen(boolean isClassPath) throws Exception {
+		if(certs.containsKey("signGen")) {
+			return (SMIMESignedGenerator) certs.get("signGen");
+		}
+		try {
+			// -keystore "D:/Yash/MyServer.jks" -storetype JKS -keypass password -storepass password
+		InputStream inputStream = getCerFileStream(isClassPath);
+		String password = EMail.ALIAS_PASSWORD.getValue();
+		Log4J.log("FIS : "+inputStream.available()+ " toString() : "+ inputStream.toString());
+		
+		// cryptographicOperations
+		certs = BC_Cryptography.unLockToGetCertificates(inputStream, password);
+		if (!certs.isEmpty()) {
+			X509Certificate signCert = (X509Certificate) certs.get("signCert");
+			PrivateKey privateKey_PEM = (PrivateKey) certs.get("privateKey");
+			KeyStore keyStore = (KeyStore) certs.get("keystore");
+			
+			SMIMESignedGenerator gen = BC_Cryptography.buildSignedGenerator(signCert, privateKey_PEM, keyStore, true);
+			
+			certs.put("signGen", gen);
+			return gen;
+		}
+		
+		} catch (Exception e) {
+			throw new Exception(e);
+		}
+		return null;
+	}
+	
 	public static Session getSessionObject(boolean auth, String protocal) {
 		if(protocal.equalsIgnoreCase("SMTPS") || protocal.equalsIgnoreCase("SMTP") ) {
 			return MailSession.getTransport(auth, protocal);
@@ -165,7 +261,8 @@ public class MailOperations {
 		return tempFile;
 	}
 	
-	private static MimeMultipart setSMIMEDigitallySignedData(boolean isClassPath, String mailMessage) throws Exception {
+	static SMIMESignedGenerator signedGen;
+	private static MimeMultipart setSMIMEDigitallySignedData(boolean isClassPath) throws Exception {
 		try {
 			// -keystore "D:/Yash/MyServer.jks" -storetype JKS -keypass password -storepass password
 		InputStream inputStream = getCerFileStream(isClassPath);
@@ -184,12 +281,16 @@ public class MailOperations {
 			// generate() method to create a CMS signed-data object, which also carries a CMS signature.
 			// SecretMessage + Certificate(smime.p7s) = Multipart
 			MimeMultipart signedParts = new MimeMultipart();
-				
+			
 			// Signing Part - Encoding: base64
 			MimeBodyPart content = new MimeBodyPart();
-			String signedMsg = BC_Cryptography.getSignedMessage(signCert, privateKey_PEM, secretMessage);
-			
-			content.setText( signedMsg );
+			if (signSecretMessage) {
+				String signedMsg = BC_Cryptography.getEncodedASNMessage(signCert, privateKey_PEM, secretMessage);
+				content.setText( signedMsg );
+			} else {
+				content.setText( secretMessage );
+			}
+			signedGen = gen;
 			signedParts = gen.generate(content);
 			
 			return signedParts;
@@ -204,14 +305,18 @@ public class MailOperations {
 	
 	public static MimeBodyPart setAttachement() throws MessagingException, IOException {
 		MimeBodyPart fileBodyPart = new MimeBodyPart();
-		DataSource source = new javax.activation.FileDataSource( EMail.AttachmentFile.getValue() );
+		DataSource source = null;
 		if (useClassPath) {
-			source = new javax.activation.FileDataSource( getFileFromStream() );
+			File fileFromStream = getFileFromStream();
+			source = new javax.activation.FileDataSource( fileFromStream );
 		} else {
 			source = new javax.activation.FileDataSource( EMail.AttachmentFile.getValue() );
 		}
 		fileBodyPart.setDataHandler( new DataHandler( source ) );
-		fileBodyPart.setFileName( EMail.AttachmentFile.getValue() );
+		fileBodyPart.setFileName( source.getName() );
+		
+		//InputStream is = attachment.getContent();
+		//DataSource attachementDataSource = new ByteArrayDataSource(is, attachment.getContentType(), attachment.getName());
 		return fileBodyPart;
 		
 	}
